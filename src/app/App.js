3b9417451d87303b8e8f4722a36a089e39d04e27
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-19 04:58:02 -0500

    Adjust typographic line-height

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h2>This is app</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque itaque molestias consectetur, minus provident, dolor ducimus libero commodi perspiciatis quibusdam explicabo ut unde ea quod consequatur ipsa quos nam possimus?</p>
     </div>
   );
 }
